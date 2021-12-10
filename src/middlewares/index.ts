e6492cec22fd5f5982f0356063ec1732f6509864
戴海洋 <1037306928@qq.com>
2021-12-11 01:34:07 +0800

    修改中间件方法顺序

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -30,10 +30,6 @@ const mdStatic: Middleware<Context> = KoaStatic(path.join(process.cwd(), '/publi
 	defer: true, // 如果为true，则在返回next()之后进行服务，从而允许后续中间件先进行响应
 })
 
-// 统一返回格式 错误处理
-const mdResHandler: Middleware<Context> = response()
-const mdErrorHandler: Middleware<Context> = error()
-
 // 参数解析中间件
 // koa-bodyparser 这个插件只能解析 4 种数据[ 'json', 'form', 'text', 'xml' ]，当我们上传文件的时候，我们是获取不到文件的。所以使用formidable
 const mdFormidable: Middleware<Context> = formidable()
@@ -49,12 +45,17 @@ const mdBodyparser: Middleware<Context> = bodyparser({
 // 记录请求日志
 const mdLogger: Middleware<Context> = logger.use()
 
+// 统一返回格式 错误处理
+const mdResHandler: Middleware<Context> = response()
+const mdErrorHandler: Middleware<Context> = error()
+
 // 路由中间件
 const mdProtectedRoute: Router.Middleware = protectedRouter.routes()
 const mdProtectedRouterAllowed: Router.Middleware = protectedRouter.allowedMethods()
 const mdUnProtectedRoute: Router.Middleware = unprotectedRouter.routes()
 const mdUnProtectedRouterAllowed: Router.Middleware = unprotectedRouter.allowedMethods()
 
+// jwt鉴权
 const mdJWT = jwt
 
 // 按顺序排列好否则失效
